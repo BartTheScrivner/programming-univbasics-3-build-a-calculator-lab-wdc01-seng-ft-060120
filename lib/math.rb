43cc66afa4ce29bdc616a9a0e418896d16c51c0c
fantastic-scope-1514 <dframe22@gmail.com>
2020-03-25 00:16:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,17 +7,17 @@ def subtraction(num1, num2)
   answer = num1-num2
   answer
 end
-  
+
 def division(num1, num2)
   answer = num1/num2
   answer
 end
-  
+
 def multiplication(num1, num2)
   answer = num1*num2
   answer
 end
-  
+
 def modulo(num1, num2)
   answer = num1%num2
   answer
